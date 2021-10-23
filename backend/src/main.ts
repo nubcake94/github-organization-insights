a77301853c66ef4841007f957ac58af351758dc9
Szabó Patrik <nobelkuchen@gmail.com>
2021-10-23 13:05:46 +0200

    logging added to main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
+	const logger = process.env.NODE_ENV === 'production' ? console : new Logger('bootstrap');
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	await app.listen(PORT);
+	logger.log(`Application is listening on port ${PORT}`);
 }
 bootstrap();
