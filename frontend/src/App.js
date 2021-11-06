aaaedd7a1da839d2dd2a02fd3d22602b85aaa787
Szabó Patrik <nobelkuchen@gmail.com>
2021-11-06 15:18:08 +0100

    theming skeleton added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
+import { ThemeProvider } from '@material-ui/core';
 import routes from 'app/routes';
 import { persistor, store } from 'app/store/store';
+import theme from 'app/theme';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import './App.css';
 
 export const history = createBrowserHistory({
 	basename: process.env.PUBLIC_URL,
@@ -23,7 +24,9 @@ function RootApp() {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor} onBeforeLift={() => {}}>
-				<App />
+				<ThemeProvider theme={theme}>
+					<App />
+				</ThemeProvider>
 			</PersistGate>
 		</Provider>
 	);
---
+++ frontend/src/app/theme/palette.js
@@ -0,0 +1,12 @@
+export default {
+	primary: {
+		main: 'red',
+	},
+	secondary: {
+		main: 'blue',
+	},
+	error: {},
+	text: {},
+	background: {},
+	divider: {},
+};
