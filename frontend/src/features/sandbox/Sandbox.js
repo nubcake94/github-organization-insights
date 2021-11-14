6f77b31b6b74acea691eb9354d57c4d3e40fb080
Szabó Patrik <nobelkuchen@gmail.com>
2021-11-14 17:02:03 +0100

    Typography edited, sandbox button added

--- frontend/src/features/sandbox/Sandbox.js
+++ frontend/src/features/sandbox/Sandbox.js
@@ -1,8 +1,18 @@
-import { makeStyles, Typography } from '@material-ui/core';
+import { Button, makeStyles, Typography } from '@material-ui/core';
+import GetAppIcon from '@material-ui/icons/GetApp';
 import { Page } from 'app/components';
+import axiosService from 'app/store/axiosService';
+import palette from 'app/theme/palette';
 
 const Sandbox = () => {
 	const classes = useStyles();
+
+	const getAllOrganizations = () => {
+		axiosService.instance.get('/organization').then((response) => {
+			console.log(response);
+		});
+	};
+
 	return (
 		<Page title="Homokozó" className={classes.mainContainer}>
 			<Typography variant="h1">Sandbox here!</Typography>
@@ -10,6 +20,15 @@ const Sandbox = () => {
 			<Typography variant="h3">Sandbox here!</Typography>
 			<Typography variant="h4">Sandbox here!</Typography>
 			<Typography variant="h5">Sandbox here!</Typography>
+			<Typography>Sandbox here!</Typography>
+			<Typography variant="subtitle1">Sandbox here!</Typography>
+			<Button
+				startIcon={<GetAppIcon />}
+				className={classes.button}
+				onClick={getAllOrganizations}
+			>
+				Get all organizations
+			</Button>
 		</Page>
 	);
 };
@@ -21,6 +40,12 @@ const useStyles = makeStyles((theme) => ({
 			paddingLeft: '16px',
 			paddingRight: '16px',
 		},
+	},
+	button: {
+		margin: 16,
+		padding: '8px 24px 8px 24px',
+		borderRadius: 4,
+		backgroundColor: palette.background.paperLighter,
 	},
 }));
 
