a5924456ee9bd420e0169eea72c16324424a479e
Szabó Patrik <nobelkuchen@gmail.com>
2021-11-16 21:58:05 +0100

    First graphql request

--- backend/src/modules/organization/organization.service.ts
+++ backend/src/modules/organization/organization.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { gql, GraphQLClient } from 'graphql-request';
 import { GithubToken } from 'src/types/githubToken.type';
 import { GithubService } from '../client/github.service';
 
@@ -9,6 +10,29 @@ export class OrganizationService {
 	constructor(private readonly githubService: GithubService) {}
 
 	async getAll(githubToken: GithubToken) {
-		return { data: githubToken };
+		const client = new GraphQLClient('https://api.github.com/graphql');
+
+		const query = gql`
+			query {
+				__type(name: "Repository") {
+					name
+					kind
+					description
+					fields {
+						name
+					}
+				}
+			}
+		`;
+
+		const data = await client.request(
+			query,
+			{},
+			{ Authorization: `Bearer ${githubToken.access_token}` },
+		);
+
+		console.log(data);
+
+		return data;
 	}
 }
