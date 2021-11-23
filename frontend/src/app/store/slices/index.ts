7db9727843b1ff62610b7c8f6fe25fb903418909
Szabó Patrik <nobelkuchen@gmail.com>
2021-11-23 17:08:26 +0100

    RepositorySlice added

--- frontend/src/app/store/slices/index.ts
+++ frontend/src/app/store/slices/index.ts
@@ -1,3 +1,4 @@
 export { default as authReducer } from './auth.slice';
 export { default as organizationReducer } from './organization.slice';
 export { default as profileReducer } from './profile.slice';
+export { default as repositoryReducer } from './repositories.slice';
---
+++ frontend/src/app/store/slices/repositories.slice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axiosService from '../axiosService';
+import { errorToast } from '../toast';
+
+export const fetchCollaboratedRepos = createAsyncThunk(
+	'fetchCollaboratedRepos',
+	async ({ organizationLogin }) => {
+		try {
+			const { data } = await axiosService.instance.get(`/organization/${organizationLogin}`);
+			return data;
+		} catch (error) {
+			errorToast('A repository-k lekérdezése nem sikerült!');
+			throw error;
+		}
+	}
+);
+
+const repositorySlice = createSlice({
+	name: 'repository',
+	initialState: {
+		repositories: null,
+		selectedIndex: null,
+		isLoading: false,
+	},
+	reducers: {
+		selectRepository: (state, action) => {
+			state.selectedIndex = action.payload;
+		},
+	},
+	extraReducers: {
+		[fetchCollaboratedRepos.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[fetchCollaboratedRepos.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.organizations = action.payload;
+		},
+	},
+});
+
+export const selectRepositories = ({ repository: { repositories, selectedIndex, isLoading } }) => ({
+	repositories,
+	selectedIndex,
+	isLoading,
+});
+
+export const { selectRepository } = repositorySlice.actions;
+
+export default repositorySlice.reducer;
